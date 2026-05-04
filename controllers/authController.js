8b70298dd379de3b43214185eda1747ca979b741
Foysal <foysal@example.com>
2026-05-04 19:09:49 +0600

    add domain

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,8 @@ const createSendToken = (user, statusCode, res) => {
       expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
       httpOnly: true,
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-      secure: process.env.NODE_ENV === 'production' ? true : false
+      secure: process.env.NODE_ENV === 'production' ? true : false,
+      domain: undefined
    };
 
    res.cookie('jwt', token, cookieOptions);
