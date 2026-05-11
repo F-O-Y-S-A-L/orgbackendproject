60af75f963323c143dac8040d764ed207d123ac4
Foysal <foysal@example.com>
2026-05-11 15:17:01 +0600

    app file update

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ const createSendToken = (user, statusCode, res) => {
 
    const cookieOptions = {
       expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-      httpOnly: true,
+      httpOnly: false,
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       secure: process.env.NODE_ENV === 'production' ? true : false,
       domain: undefined
